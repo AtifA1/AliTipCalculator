bda416d0e2d58258bc2c7320b19d4395b74969af
BT_2N2_01 <sample@bths.edu>
2023-09-21 13:37:09 -0400

    Added total before tip

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -2,10 +2,26 @@ import java.util.Scanner;
 public class TipCalculator {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        System.out.print("Welcome to the tip calculator!");
+        System.out.print("Welcome to the tip calculator!\n");
+
         System.out.print("How many people are in your group: ");
         int people = scan.nextInt();
 
+        System.out.print("What's the tip percentage? (0-100): ");
+        int tip = scan.nextInt();
+
+        double totalBeforeTip = 0;
+
+        while (people != 0) {
+            System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (-1 to the end): ");
+            double cost = scan.nextDouble();
+            if (cost != -1) {
+                totalBeforeTip += cost;
+            }
+            people--;
+        }
+        System.out.println("The total before tips is: $" + totalBeforeTip);
+        System.out.print("Total percentage: " + tip);
 
     }
 }
